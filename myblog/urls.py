ad30220180d5726002253bd3a8f172433604ecfb
bonowg <bonowg@gmail.com>
2017-07-07 21:01:02 +0200

    poprawa importu 2

--- myblog/urls.py
+++ myblog/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-import blog
+from blog import urls
 from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include(blog.urls)),
+    url(r'', include(urls)),
 ]
