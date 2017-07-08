0e4c4fc94cc830953dd419b74bf97fef2cfe448a
bonowg <bonowg@gmail.com>
2017-07-08 22:12:39 +0200

    dodanie querysetow

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import post
+from django.utils import timezone
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = post.objects.filter(published_date_lte=timezone.now().order_by('published_date'))
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
